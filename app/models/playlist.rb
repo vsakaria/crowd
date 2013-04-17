98d403650b710572cde1af9636c82e91175044bb
railyboy <s.m.github@gmail.com>
2013-04-17 12:10:04 +0100

    Annotate models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,24 @@
+# == Schema Information
+#
+# Table name: playlists
+#
+#  id          :integer          not null, primary key
+#  artist_id   :string(255)
+#  artist_name :string(255)
+#  title       :string(255)
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Playlist < ActiveRecord::Base
   attr_accessible :artist_id, :artist_name, :id, :title, :user_id
 
   belongs_to :user
+
+  def new
+
+  end
+
 end
 
