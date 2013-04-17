1db7ba89741a08cf1ebb5c594a8d53eed12dc8c2
Vishal Sakaria <vsakaria@gmail.com>
2013-04-17 10:23:34 +0100

    All test complete for the Search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,13 +6,21 @@ class Search
   attr_accessor :artist, :track
 
   def initialize artist
-    @artist = artist
+    if !artist.empty?
+      @artist = artist
+    else
+      @artist = nil
+    end
   end
 
   def do_search
+    if @artist
       @result = Curl::Easy.perform("http://developer.echonest.com/api/v4/song/search?api_key=JMEUQC5ZW6CHVBCXG&artist=#{@artist}").body_str
 
       parse_json(@result)
+    else
+      nil
+    end
   end
 
 
