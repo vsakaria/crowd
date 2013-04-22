0b8861c2a201ace7475241ca83c4d378231f710b
Vishal Sakaria <vsakaria@gmail.com>
2013-04-22 12:57:01 +0100

    Rdio play button workiing

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,1 +1,19 @@
 
+$(function() {
+
+  $('#rdio').rdio("GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=");
+
+
+    $('#rdio').bind('ready.rdio', function() {
+
+      console.log("Rdio init");
+
+    });
+
+
+
+});//Doc ready end
+
+
+ //
+ //           $.rdio().play('t2732487')
--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -5,8 +5,20 @@ $(function() {
 
   $("form#search-box").on("ajax:success", function(){
 
-  $( 'div#search-results ul:not(:last-child)' ).remove();
+        $( 'div#search-results ul:not(:last-child)' ).remove();
 
+      $('#play_button').click( function() {
+          console.log("Click play on MAx own our search event handle");
+          $('#rdio').rdio().play('t3561638')
+      });
+
+      $('#stop_button').click( function() {
+          console.log("Click stop event handle");
+          $('#rdio').rdio().stop()
+      });
   });
 
 });
+
+
+//Only play tracj id's
