997ad1d4c95accf95d58df05bf1eb53d954099c0
railyboy <s.m.github@gmail.com>
2013-04-25 14:11:16 +0100

    Major refactor of search - remove Echonest, replace with Rdio

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,18 +1,20 @@
 $(function() {
-  console.log("Success callback handle")
+  console.log("Success callback handle");
   $("form#search-box").on("ajax:success", function(){
     $('div#search-results ul:not(:last-child)').remove();
 
-    $('.play-rdio-track').on('click', function() {
+    $('.play-rdio-track').on('click', function(e) {
+      e.preventDefault();
       console.log("Click play on MAx own our search event handle");
       var rdio_id = $(this).attr('id');
+      $('#rdio').rdio().stop();
       $('#rdio').rdio().play(rdio_id);
       console.log(rdio_id + " should be playing");
     });
 
     $('.stop-rdio-track').click( function() {
       console.log("Click stop event handle");
-      $('#rdio').rdio().stop()
+      $('#rdio').rdio().stop();
     });
   });
 });
